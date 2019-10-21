57fc5dabe509c6fd2183591d963e069c1824d674
fralps <francois.loupias@orange.fr>
2019-10-21 15:54:55 +0200

    Adding 10 users in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Colorize just for fun
+require 'colorize'
+
+# Faker gem
+require 'faker'
+
+# To limit the database entries
+# each time we seed we delete the previous ones and we reset database's ids
+require 'database_cleaner'
+
+DatabaseCleaner.clean_with(:truncation)
+
+User.destroy_all
+
+# Create 10 users for development
+10.times do
+  user = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+
+  # Puts to show every time an user is created
+  puts 'User created'.green
+end
